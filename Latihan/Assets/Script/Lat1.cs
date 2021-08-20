3f7bb88d6790d064bded1bbf655d9aec56f6e772
WinaNisatulM <wnm.tp20@mail.mmtc.ac.id>
2021-08-20 17:06:00 +0700

    Latihannnn

---
+++ Latihan/Assets/Script/Lat1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lat1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+      Debug.Log("Awake Dipanggil");
+    }
+
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Waktu untuk Update :" + Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+       Debug.Log("Waktu untuk FixedUpdate :" + Time.deltaTime);
+    }
+
+    void LateUpdate()
+    {
+     Debug.Log("Waktu untuk LateUpdate :" + Time.deltaTime);
+    }
+
+}
+
---
+++ Latihan/Assets/Script/Lat2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lat2 : MonoBehaviour
+{
+    public int nilaiA;
+    public int nilaiB;
+
+    [Range(1, 5)]
+    public int nilaiC;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      int totall = (nilaiA + nilaiB) * nilaiC;
+      Debug.Log("Total "+totall);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
